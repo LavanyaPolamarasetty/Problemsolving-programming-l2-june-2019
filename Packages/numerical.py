ed962b19cb6f54f54d18868d2422b5ec6292eb90
mercypolamarasetty <lavanya.p2211@gmail.com>
2019-06-18 18:42:33 +0530

    18-june file added

---
+++ Packages/numerical.py
@@ -0,0 +1,33 @@
+
+
+
+
+# find out isprime
+def isprime(i):
+    q=0
+    for u in range(1,i):
+        if i%u==0:
+            q+=1
+            #print(u)
+    if q<=2:
+        return True
+    else:
+        return False
+
+# find out how many char & digits in a given string
+
+u=input()
+s=[]
+c=0
+dc=0
+up=0
+l=0
+for i in u:
+    if i.isalpha():
+        if i.isupper():
+            up+=1
+        elif i.islower():
+            l+=1
+    elif i.isdigit():
+            dc+=1
+print(dc,l,up)
